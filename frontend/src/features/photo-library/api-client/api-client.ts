6173d16b3060ba9adabfaf55390c916b91e46b3b
Nicolas <nicolas.feith@gmail.com>
2023-09-06 01:00:16 +0200

    feat(frontend): Implement search function to call api using JS fetch

--- frontend/src/features/photo-library/api-client/api-client.ts
+++ frontend/src/features/photo-library/api-client/api-client.ts
@@ -1,7 +1,10 @@
 export const ApiClient = {
   search: async (query: string) => {
-    //TODO implement
-    return [];
+    const response = await fetch(`https://localhost:8000/image/search?${query}`, {
+      method: 'GET',
+    })
+    const data = await response.json()
+    return data.items.map((item: any) => item.name);
   },
 
   upload: async (file: File) => {
